f801f81ce05a8559740ba28687eb52a3f74b7bf6
João Nini <105883412+JoaoNini75@users.noreply.github.com>
2024-08-05 19:44:32 +0100

    Add files via upload

--- scriptv2.js
+++ scriptv2.js
@@ -1,4 +1,4 @@
-const btn = document.querySelector("#btn");
+const calculateBtn = document.querySelector("#btn");
 const inputs = document.querySelectorAll("input");
 const INPUT_LEN = inputs.length;
 const INVALID_INPUT = "As notas válidas variam entre 10 e 20. Corrige os campos que não cumprem este critério.";
@@ -7,16 +7,17 @@ let semesterECTS, semesterGrades, yearECTS, yearGrades, areaECTS, areaGrades;
 let totalGrades, totalECTS;
 
 
-btn.addEventListener("click", calculate);
+calculateBtn.addEventListener("click", calculate);
 
 function calculate() {
-    initalizeArrays();
+    initalizeVariables();
 
     if (!isInputValid())
         alert(INVALID_INPUT);
 
     acumulateGradesAndECTS();
-    printResultsConsole();
+    //printResultsConsole();
+    displayResults();
 }
 
 function printResultsConsole() {
@@ -32,11 +33,12 @@ function printResultsConsole() {
 
     console.log("Áreas:");
     for (let i = 0; i < 7; i++)
-        console.log(areaIndexToName(i) + " " + areaGrades[i] / areaECTS[i]);
+        console.log(areaIndexToAcronym(i) + " " + areaGrades[i] / areaECTS[i]);
     console.log("");
 }
 
-function initalizeArrays() {
+function initalizeVariables() {
+    totalGrades = totalECTS = 0;
     semesterECTS = Array(10).fill(0);
     semesterGrades = Array(10).fill(0);
     yearECTS = Array(5).fill(0);
@@ -84,6 +86,29 @@ function acumulateGradesAndECTS() {
     }
 }
 
+function displayResults() {
+    console.log(totalGrades + " " + totalECTS);
+
+    let average = totalGrades / totalECTS;
+    document.getElementById("res_geral").innerText = "Média Geral: " + (isNaN(average) ? "" : average.toFixed(2));
+
+    for (let i = 0; i < 10; i++) {
+        average = semesterGrades[i] / semesterECTS[i];
+        document.getElementById("sem" + (i+1)).innerText = isNaN(average) ? "" : average.toFixed(2);
+    }
+        
+    for (let i = 0; i < 5; i++) {
+        average = yearGrades[i] / yearECTS[i];
+        document.getElementById("ano" + (i+1)).innerText = isNaN(average) ? "" : average.toFixed(2);
+    }
+
+    for (let i = 0; i < 7; i++) {
+        average = areaGrades[i] / areaECTS[i];
+        document.getElementById("area" + (i+1)).innerText = areaIndexToName(i) + ": " + (isNaN(average) ? "" : average.toFixed(2));
+    }
+}
+
+
 function areaNameToIndex(area) {
     switch (area) {
         case "i": return 0;
@@ -96,7 +121,7 @@ function areaNameToIndex(area) {
     }
 }
 
-function areaIndexToName(index) {
+function areaIndexToAcronym(index) {
     switch (index) {
         case 0: return "i";
         case 1: return "m";
@@ -108,3 +133,14 @@ function areaIndexToName(index) {
     }
 }
 
+function areaIndexToName(index) {
+    switch (index) {
+        case 0: return "Informática";
+        case 1: return "Matemática";
+        case 2: return "Eletrotécnica e de Computadores";
+        case 3: return "Física";                 
+        case 4: return "Competências Complementares";
+        case 5: return "Ciências Humanas e Sociais";
+        case 6: return "Bloco Livre";
+    }
+}
